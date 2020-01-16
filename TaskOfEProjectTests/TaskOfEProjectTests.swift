786b1bc3b96e5c95819b771bf8ab4be8efd628df
Nermeen <nrmeen.tomoum@alumni.fci-cu.edu.eg>
2020-01-16 13:06:26 +0400

    UITest

--- TaskOfEProjectTests/TaskOfEProjectTests.swift
+++ TaskOfEProjectTests/TaskOfEProjectTests.swift
@@ -9,26 +9,37 @@
 import XCTest
 @testable import TaskOfEProject
 
-class TaskOfEProjectTests: XCTestCase {
-
+class WeatherTests: XCTestCase {
+    
     override func setUp() {
+        super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    func testExtendedInfoTitleStrings() {
+        
+        let feelLikeInfo = ExtendedInfo.feelLike.stringValue
+        XCTAssertEqual(feelLikeInfo, "FEEL LIKE:  ")
+        
+        let chanceOfRainInfo = ExtendedInfo.chanceOfRain.stringValue
+        XCTAssertEqual(chanceOfRainInfo, "CHANCE OF RAIN:  ")
+        
+        let uvIndexInfo = ExtendedInfo.uvIndex.stringValue
+        XCTAssertEqual(uvIndexInfo, "UV INDEX:  ")
+        
+        let windIndexInfo = ExtendedInfo.wind.stringValue
+        XCTAssertEqual(windIndexInfo, ExtendedInfo.wind.rawValue.uppercased() + ":  ")
+    }
 }
